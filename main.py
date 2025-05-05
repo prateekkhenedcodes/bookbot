4afd5e80f80747db85b222142297c6c2a51d1477
suresh <code.prateekkhened@gmail.com>
2025-05-06 01:14:04 +0530

    add to count number of repeatative characters

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from stats import get_num_words, get_book_text
+from stats import get_num_words, get_book_text, get_num_characters
 
 def main():
     book_path = "books/frankenstein.txt"
+
     book_content = get_book_text(book_path)
+
+
     num_words = get_num_words(book_content)
     print(f"{num_words} words found in the document")
+
+
+    num_char = get_num_characters(book_content)
+    print(num_char)
+    # for key, value in num_char:
+
 
 
 
--- stats.py
+++ stats.py
@@ -5,3 +5,15 @@ def get_book_text(book_path):
 
 def get_num_words(book_content):
     return len(book_content.split())
+
+
+def get_num_characters(book_content):
+    my_dict = {}
+    for char in book_content:
+        char = char.lower()
+        if char in my_dict:
+            my_dict[char] += 1
+        else:
+            my_dict[char] = 1
+    return my_dict
+            
